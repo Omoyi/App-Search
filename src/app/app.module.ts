15b80f3d7de543fe4629586b080d35b2e672634c
Uwimana Rachel <urakhel7@gmail.com>
2019-08-30 16:46:18 +0200

    import a httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UtilizersPortraitComponent } from './utilizers-portrait/utilizers-portrait.component';
@@ -12,7 +12,8 @@ import { UtilizersPortraitComponent } from './utilizers-portrait/utilizers-portr
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
