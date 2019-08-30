c68ca9cfe36f5717bfc5d1a4510f050cebe2e657
Uwimana Rachel <urakhel7@gmail.com>
2019-08-30 17:44:31 +0200

    rewrite and add \some codes

--- src/app/portrait-class/portrait.ts
+++ src/app/portrait-class/portrait.ts
@@ -1,2 +1,5 @@
 export class Portrait {
+  constructor (avatar_url: string, name: string, login: string, public_repos: number,
+    created_at: Date){
+  }
 }
--- src/app/utilizers-portrait/utilizers-portrait.component.ts
+++ src/app/utilizers-portrait/utilizers-portrait.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Portrait } from '../portrait-class/portrait';
+import { HttpClient } from '@angular/common/http'
 @Component({
   selector: 'app-utilizers-portrait',
   templateUrl: './utilizers-portrait.component.html',
   styleUrls: ['./utilizers-portrait.component.css']
 })
 export class UtilizersPortraitComponent implements OnInit {
-
-  constructor() { }
+  
+  profile: Portrait;
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    interface ApiResult{
+      avatar_url: string;
+      name: string;
+      login: string;
+      public_repos: number;
+      created_at: Date
+      
+    }
+    
+    this.http.get<ApiResult>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+      // Succesful API request
+      this.profile = new Portrait(data.avatar_url,data.name, data.login, data.public_repos, data.created_at)
+    })
+
   }
 
 }
