5eb816656d8a99ee264e4a631785096728075efa
Uwimana Rachel <urakhel7@gmail.com>
2019-08-30 17:11:17 +0200

    delete the portait class

--- src/app/portrait.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Portrait } from './portrait';
-
-describe('Portrait', () => {
-  it('should create an instance', () => {
-    expect(new Portrait()).toBeTruthy();
-  });
-});
--- src/app/portrait.ts
+++
@@ -1,8 +0,0 @@
-import { Url } from 'url';
-
-export class Portrait {
-  constructor (avatar_url: Url, name: string, login: string, public_repos: number,
-  created_at: Date, ){
-
-  }
-}
