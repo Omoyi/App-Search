081ccaada804b23bd2806f262c55fe1df3832b07
Omoyi <urakhel7@gmail.com>
2019-09-02 13:31:32 +0200

    add a loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UtilizersPortraitComponent } from './utilizers-portrait/utilizers-portrait.component';
 import { DateCountPipe } from './date-count.pipe';
-import { PortraitService } from 'src/app/portrait.service'
+import { PortraitService } from 'src/app/portrait.service';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { PortraitService } from 'src/app/portrait.service'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
