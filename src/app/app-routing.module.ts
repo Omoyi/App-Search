9a1d602d79b55be6fce5d65b8d9149346d1da99e
Omoyi <urakhel7@gmail.com>
2019-09-03 03:53:00 +0200

    add routes and do further styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UtilizersPortraitComponent } from 'src/app/utilizers-portrait/utilizers-portrait.component';
+import { TheUserComponent } from 'src/app/the-user/the-user.component';
+import { ViewComponent } from 'src/app/view/view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ViewComponent},
+  { path: 'user/repositories?', component: UtilizersPortraitComponent},
+  { path: 'userprofile?', component: TheUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
